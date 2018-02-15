76a4381ca229afe91e3ff1d3d139dec83f24a973
James N. V. Cash <james.nvc@gmail.com>
2018-02-15 16:15:18 -0500

    Change github repo name

--- pack.pl
+++ pack.pl
@@ -2,5 +2,5 @@ name(msgpack).
 title('DCG for packing & unpacking of MessagePack data to/from bytes').
 version('0.1.0').
 author('James N. V. Cash', 'james.cash@occasionallycogent.com').
-home('https://github.com/jamesnvc/prolog-messagepack').
-download('https://github.com/jamesnvc/prolog-messagepack/releases/*.zip').
+home('https://github.com/jamesnvc/msgpack').
+download('https://github.com/jamesnvc/msgpack/releases/*.zip').
