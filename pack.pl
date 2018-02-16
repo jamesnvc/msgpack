389e24e5ad22db93f0eedf2758799162643f435d
James N. V. Cash <james.nvc@gmail.com>
2018-02-16 09:41:29 -0500

    Release: "Fast enough to use"

--- pack.pl
+++ pack.pl
@@ -1,6 +1,6 @@
 name(msgpack).
 title('DCG for packing & unpacking of MessagePack data to/from bytes').
-version('0.1.2').
+version('0.1.4').
 author('James N. V. Cash', 'james.cash@occasionallycogent.com').
 home('https://github.com/jamesnvc/msgpack').
 download('https://github.com/jamesnvc/msgpack/releases/*.zip').
