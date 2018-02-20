41670038f48cd28d81bb104c49d32a9c5460e24b
James N. V. Cash <james.nvc@gmail.com>
2018-02-20 13:41:42 -0500

    Bump version to have fixed signed/unsigned conversion

--- pack.pl
+++ pack.pl
@@ -1,6 +1,6 @@
 name(msgpack).
 title('DCG for packing & unpacking of MessagePack data to/from bytes').
-version('0.1.4').
+version('0.1.5').
 author('James N. V. Cash', 'james.cash@occasionallycogent.com').
 home('https://github.com/jamesnvc/msgpack').
 download('https://github.com/jamesnvc/msgpack/releases/*.zip').
